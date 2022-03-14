d9a5efabfa754fb7537d7f5073c743926a28cb34
Florian Cadoz <fcadoz@waycom.net>
2022-03-14 19:26:04 +0100

    Rework

--- public/main.js
+++
@@ -1,7 +0,0 @@
-function getAll() {
-  fetch("/appart")
-    .then((response) => response.json())
-    .then((data) => {
-      console.log('data: ', data);
-    });
-}
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require("express");
 const mongoose = require('mongoose');
 const helmet = require('helmet');
+const cors = require('cors');
 const compression = require('compression');
 const routes = require('./routes/appart');
 
@@ -28,7 +29,19 @@ mongoose.connect(
 
 
 /* MIDDLEWARE */
-app.use(helmet());
+app.use(helmet({
+  contentSecurityPolicy: false,
+}));
+app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, HEAD, OPTIONS, PUT, PATCH, DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token, x-refresh-token, _id");
+
+  res.header('Access-Control-Expose-Headers', 'x-access-token, x-refresh-token');
+
+  next();
+});
 app.use(compression());
 app.use(express.static("public"));
 app.use(express.json());
