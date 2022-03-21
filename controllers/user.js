0a430b7e97a964ee29bb4c0dd46abc3a147ee9b5
Florian Cadoz <fcadoz@waycom.net>
2022-03-21 16:21:03 +0100

    Login enhanced

--- controllers/user.js
+++ controllers/user.js
@@ -29,14 +29,18 @@ const login = (req, res) => {
 
   User.findOne({name: userData.name})
     .then(user => {
-      let dbPsw = cryptoJS.AES.decrypt(user.password, process.env.SECRET_PASSWORD).toString(cryptoJS.enc.Utf8);
-      let userPsw = cryptoJS.AES.decrypt(userData.password, process.env.SECRET_PASSWORD).toString(cryptoJS.enc.Utf8);
-      if (!user || dbPsw !== userPsw) {
+      if (!user) {
         res.sendStatus(401);
       } else {
-        let payload = { subject: user._id };
-        let token = jwt.sign(payload, process.env.SECRET_KEY);
-        res.json({token});
+        let dbPsw = cryptoJS.AES.decrypt(user.password, process.env.SECRET_PASSWORD).toString(cryptoJS.enc.Utf8);
+        let userPsw = cryptoJS.AES.decrypt(userData.password, process.env.SECRET_PASSWORD).toString(cryptoJS.enc.Utf8);
+        if (dbPsw !== userPsw) {
+          res.sendStatus(401);
+        } else {
+          let payload = { subject: user._id };
+          let token = jwt.sign(payload, process.env.SECRET_KEY);
+          res.json({token});
+        }
       }
     })
     .catch(err => {
