43317844d23fc8a0fda599c433442265c57ba5e4
Florian Cadoz <fcadoz@waycom.net>
2022-03-23 15:18:14 +0100

    Human unique code added to AppartSchema

--- models/appart.model.js
+++ models/appart.model.js
@@ -1,6 +1,21 @@
 const mongoose = require("mongoose");
 
 const AppartSchema = new mongoose.Schema({
+  code: {
+    type:String,
+    default: function() {
+      const now = new Date();
+      const yyyy = now.getFullYear();
+      let mm = now.getMonth() + 1, dd = now.getDate();
+      if (dd < 10) dd = `0${dd}`;
+      if (mm < 10) mm = `0${mm}`;
+      const HH = now.getHours();
+      const MM = now.getMinutes();
+      const clearedName = this.name.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+
+      return `${dd}${mm}${yyyy}-${HH}${MM}-${clearedName}`;
+    }
+  },
   name: {
     type:String,
     minlength: 1,
